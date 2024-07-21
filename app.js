65661a9ec2123fe647c888b1ba6bec0352d501e7
Erick Ferreira <erickferreiradev@gmail.com>
2024-07-20 23:50:01 -0300

    Novas funções adicionadas

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
+const path = require('path')
 const db = require('./db/connections')
 const bodyParser = require('body-parser')
+const Job = require('./models/Job')
 
 const por = 3000
 
@@ -11,6 +14,14 @@ app.listen(por, function(){
 
 // body parser
 app.use(bodyParser.urlencoded({ extended: false}))
+
+// handle bars
+app.set('views', path.join(__dirname, 'views'))
+app.engine('handlebars', exphbs.create({defaultLayout: 'main'}).engine)
+app.set('view engine', 'handlebars')
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 // db connection
 
@@ -26,8 +37,16 @@ db
 
 // routes
 app.get('/', (req, res) =>{
-    res.send("Está rodando home")
 
+    Job.findAll({order: [
+        ['createdAt', 'DESC']
+    ]})
+    .then(jobs =>{
+        res.render('index',
+            jobs
+        )
+    })
+    
 })
 
 // jobs routes
